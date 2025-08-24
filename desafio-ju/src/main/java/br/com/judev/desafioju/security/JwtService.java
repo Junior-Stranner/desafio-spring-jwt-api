baff02a116e9ba6cdbba52ff35cd1951735a141e
Junior-Stranner <JuniorStranner@gmail.com>
2025-08-24 18:16:11 -0300

    criando o jwt service que irá conter a lógica para gerar e validar o meu token

---
+++ desafio-ju/src/main/java/br/com/judev/desafioju/security/JwtService.java
@@ -0,0 +1,47 @@
+package br.com.judev.desafioju.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.time.Instant;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+
+    public String generateToken(UserDetails userDetails) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            return JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(userDetails.getUsername())
+                    .withExpiresAt()
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar o token", exception);
+        }
+    }
+
+    public Instant generateExpirationDate() {
+        return Instant.now().plusSeconds(expiration);
+    }
+
+    public String validateToken(String token) {
+        try{
+
+        }catch(JWTVerificationException exception){
+
+        }
+    }
+}
