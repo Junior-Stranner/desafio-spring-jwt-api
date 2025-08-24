b0fbe1faa6bbe24c59840209d821b539bf3d35c0
Junior-Stranner <JuniorStranner@gmail.com>
2025-08-24 15:24:35 -0300

    subindo o projeto ainda sendo feito

---
+++ desafio-ju/src/main/java/br/com/judev/desafioju/DesafioJuApplication.java
@@ -0,0 +1,13 @@
+package br.com.judev.desafioju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioJuApplication.class, args);
+    }
+
+}
---
+++ desafio-ju/src/test/java/br/com/judev/desafioju/DesafioJuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.judev.desafioju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
